b92adef2ee271084af1604229a8c42a113afc651
Moore <Moores@netscout.com>
2021-06-08 15:23:44 -0500

    added exercise options and pics

--- components/AddExercise.js
+++ components/AddExercise.js
@@ -3,10 +3,7 @@ import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
 import styles from "../styles/AddExercise.module.css";
-
-// needs parent func submitNewEntry(), parent prop workoutName, showDialog, isEditing
-// needs handleChanges() locally
-// needs local pcsofSt8 exercise, duration,
+import { exerciseOptions } from "../vars/workoutVars";
 
 export default function AddExercise({ submitNewEntry, workoutName, showDialog, isEditing }) {
   const [ exercise, setExercise ] = useState("");
@@ -18,14 +15,41 @@ export default function AddExercise({ submitNewEntry, workoutName, showDialog, i
     if (name === "duration") setDuration(evt.target.value);
   }
 
+  // function handleSubmit(evt){
+  //   evt.preventDefault();
+  //   let src = exercise.replace(/\s+/g, '-').toLowerCase(); // replace spaces with dashes and convert to lowercase
+  //   let newExercise = { name: exercise.toLowerCase(), duration: duration, imgSrc: `${src}.png`, id: uuidv4() };
+  //   // call parent function submitNewEntry, pass in newExercise obj
+  //   submitNewEntry(newExercise);
+  //   setExercise(""); // clear out exercise and duration from input forms
+  //   setDuration("");
+  // }
+
   function handleSubmit(evt){
     evt.preventDefault();
-    let src = exercise.replace(/\s+/g, '-').toLowerCase(); // replace spaces with dashes and convert to lowercase
-    let newExercise = { name: exercise.toLowerCase(), duration: duration, imgSrc: `${src}.png`, id: uuidv4() };
+    // let src = exercise.replace(/\s+/g, '-').toLowerCase(); // replace spaces with dashes and convert to lowercase
+    let newSrc;
+    // build src by checking if src string has l) or r) at the end... if so, remove last 3 chars and replace with "left" or "right"
+    if (exercise[exercise.length-1] === ")" && exercise[exercise.length-2] === "L") {
+      newSrc = exercise.slice(0, exercise.length - 3).concat("left");
+    } else if (exercise[exercise.length-1] === ")" && exercise[exercise.length-2] === "R") {
+      newSrc = exercise.slice(0, exercise.length - 3).concat("right");
+    } else {
+      newSrc = exercise;
+    }
+    let newExercise = { name: exercise, duration: duration, imgSrc: `${newSrc}.gif`, id: uuidv4() };
     // call parent function submitNewEntry, pass in newExercise obj
+    // console.log(newExercise);
     submitNewEntry(newExercise);
     setExercise(""); // clear out exercise and duration from input forms
     setDuration("");
+  }
+
+  function giveOptions() {
+    let exOptions = exerciseOptions.map(elem => {
+      return <option key={uuidv4()} value={elem}>{elem}</option>
+    });
+    return exOptions;
   }
 
   return (
@@ -44,14 +68,7 @@ export default function AddExercise({ submitNewEntry, workoutName, showDialog, i
             required
           >
             <option value=""></option>
-            <option value="Rest">Rest</option>
-            <option value="Flutter Kicks">Flutter Kicks</option>
-            <option value="Squats">Squats</option>
-            <option value="Lunges">Lunges</option>
-            <option value="Plank">Plank</option>
-            <option value="Standing Leg Lifts">Standing Leg Lifts</option>
-            <option value="Reverse Crunches">Reverse Crunches</option>
-            <option value="Russian Twists">Russian Twists</option>
+            { giveOptions() }
           </select>
         </label>
         <br/>
@@ -84,3 +101,12 @@ export default function AddExercise({ submitNewEntry, workoutName, showDialog, i
     </div>
   );
 };
+
+// <option value="Rest">Rest</option>
+// <option value="Flutter Kicks">Flutter Kicks</option>
+// <option value="Squats">Squats</option>
+// <option value="Lunges">Lunges</option>
+// <option value="Plank">Plank</option>
+// <option value="Standing Leg Lifts">Standing Leg Lifts</option>
+// <option value="Reverse Crunches">Reverse Crunches</option>
+// <option value="Russian Twists">Russian Twists</option>
--- vars/workoutVars.js
+++ vars/workoutVars.js
@@ -4,18 +4,18 @@ const demoSavedWorkouts = [
     workoutName: "Demo Abs Workout",
     id: "001",
     routine: [
-      { name: "plank", imgSrc: "plank-1.png", duration: "20", id: "545726826" },
-      { name: "rest", imgSrc: "rest-1.png", duration: "5", id: "8704683683658" },
-      { name: "crunches", imgSrc: "crunch-1.png", duration: "20", id: "8794937386" }
+      { name: "hip dips", imgSrc: "hip dips.gif", duration: "20", id: "545726826" },
+      { name: "rest", imgSrc: "rest.gif", duration: "5", id: "8704683683658" },
+      { name: "crunches", imgSrc: "crunches.gif", duration: "20", id: "8794937386" }
     ]
   },
   {
     workoutName: "Demo Legs Workout",
     id: "002",
     routine: [
-      { name: "leg lifts", imgSrc: "leg-lift-1.png", duration: "20", id: "783742527425" },
-      { name: "rest", imgSrc: "rest-1.png", duration: "5", id: "76925727" },
-      { name: "lunges", imgSrc: "lunge-1.png", duration: "20", id: "989677368368" }
+      { name: "rainbow (L)", imgSrc: "rainbow left.gif", duration: "20", id: "783742527425" },
+      { name: "rainbow (R)", imgSrc: "rainbow right.gif", duration: "20", id: "7112523425" },
+      { name: "rest", imgSrc: "rest.gif", duration: "5", id: "76925727" }
     ]
   }
 ];
@@ -24,10 +24,10 @@ const exampleWorkout = {
   workoutName: "Example Routine 1",
   id: "003",
   routine: [
-    { name: "lunges", imgSrc: "my-img-src.png", duration: "30", id: "777" },
-    { name: "rest", imgSrc: "my-img-src.png", duration: "5", id: "888" },
-    { name: "crunches", imgSrc: "my-img-src.png", duration: "30", id: "999" },
-    { name: "rest", imgSrc: "my-img-src.png", duration: "5", id: "000" },
+    { name: "crunches", imgSrc: "crunches.gif", duration: "30", id: "777" },
+    { name: "rest", imgSrc: "rest.gif", duration: "5", id: "888" },
+    { name: "hip dips", imgSrc: "hip dips.gif", duration: "30", id: "999" },
+    { name: "rest", imgSrc: "rest.gif", duration: "5", id: "000" },
   ]
 };
 
@@ -35,15 +35,65 @@ const defaultWorkout = {
   workoutName: "Abs Workout 1",
   id: "004",
   routine: [
-    { name: "plank", imgSrc: "plank-1.png", duration: "20", id: "8550489383" }, // <-- setting durations to strings bc when we load from localStorage, we'll be dealing with strings
-    { name: "rest", imgSrc: "rest-1.png", duration: "5", id: "93783672727" },
-    { name: "leg lifts", imgSrc: "leg-lift-1.png", duration: "20", id: "542546457" },
+    { name: "plank", imgSrc: "plank.png", duration: "20", id: "8550489383" }, // <-- setting durations to strings bc when we load from localStorage, we'll be dealing with strings
+    { name: "rest", imgSrc: "rest.png", duration: "5", id: "93783672727" },
+    { name: "leg lifts", imgSrc: "leg-lifts.png", duration: "20", id: "542546457" },
     // { name: "rest", imgSrc: "rest-1.png", duration: "5" },
     // { name: "crunches", imgSrc: "crunch-1.png", duration: "15" },
     // { name: "rest", imgSrc: "rest-1.png", duration: "5" },
     // { name: "windshield wipers", imgSrc: "lunge-1.png", duration: "15" }
   ]
 };
+
+const exerciseOptions = [
+  "rest",
+  // LEGS exercises
+  "side plank leg lift (L)",
+  "side plank leg lift (R)",
+  "lower leg lift (L)",
+  "lower leg lift (R)",
+  "upper leg circle (L)",
+  "upper leg circle (R)",
+  "heel press leg lift (L)",
+  "heel press leg lift (R)",
+  "knee touch extension (L)",
+  "knee touch extension (R)",
+  "up and over (L)",
+  "up and over (R)",
+  "triangle leg raise (L)",
+  "triangle leg raise (R)",
+  "side leg raise (L)",
+  "side leg raise (R)",
+  "rainbow (L)",
+  "rainbow (R)",
+  "fire hydrant (L)",
+  "fire hydrant (R)",
+  "single leg circle (L)",
+  "single leg circle (R)",
+  "plie",
+  "standing gate open (L)",
+  "standing gate open (R)",
+  "leg sweep (L)",
+  "leg sweep (R)",
+  "lunge tap",
+  // ABS exercises
+  "reverse crunch leg extension",
+  "reverse crunches",
+  "spiderman plank",
+  "crossbody mountain climbers",
+  "russian twist",
+  "in and out",
+  "hip dips",
+  "plank",
+  "hundreds",
+  "crunches",
+  "up and down plank",
+  "spiderman plank",
+  "heel taps",
+  "windshield wipers",
+  "straight leg crunches",
+  "up and down plank"
+]
 
 const confettiConfig = {
   angle: 90,
@@ -61,4 +111,4 @@ const confettiConfig = {
 
 
 
-export { defaultWorkout, demoSavedWorkouts, confettiConfig, exampleWorkout };
+export { defaultWorkout, demoSavedWorkouts, confettiConfig, exampleWorkout, exerciseOptions };
