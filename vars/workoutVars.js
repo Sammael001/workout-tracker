81d2db5a10d2a09ae3f5786eae106e7bf3158d00
Moore <Moores@netscout.com>
2021-06-17 06:52:28 -0500

    css tweaks, added CT legs workout to vars

--- vars/workoutVars.js
+++ vars/workoutVars.js
@@ -17,6 +17,56 @@ const demoSavedWorkouts = [
       { name: "rainbow (R)", imgSrc: "rainbow right.gif", duration: "20", id: "7112523425" },
       { name: "rest", imgSrc: "rest.gif", duration: "5", id: "76925727" }
     ]
+  },
+  {
+    workoutName: "CT - Abs 1",
+    id: "008",
+    routine: [
+      { name: "rest", imgSrc: "rest.gif", duration: "5", id: "8285638n3bg727" },
+      { name: "side plank leg lift (L)", imgSrc: "side plank leg lift left.gif", duration: "30", id: "4j83b457bh5" },
+      { name: "rest", imgSrc: "rest.gif", duration: "5", id: "zsgsnbr5784b" },
+      { name: "lower leg lift (L)", imgSrc: "lower leg lift left.gif", duration: "30", id: "1143657jnvg245bh5" },
+      { name: "upper leg circle (L)", imgSrc: "upper leg circle left.gif", duration: "30", id: "6w58n34j83b4w57bh5" },
+      { name: "rest", imgSrc: "rest.gif", duration: "5", id: "383e5638n3bg727" },
+      { name: "heel press leg lift (L)", imgSrc: "heel press leg lift left.gif", duration: "30", id: "q346b4j83q5qh5" },
+      { name: "knee touch extension (L)", imgSrc: "knee touch extension left.gif", duration: "30", id: "4nm45682bv14" },
+      { name: "rest", imgSrc: "rest.gif", duration: "5", id: "jnn4763g2562476" },
+      { name: "up and over (L)", imgSrc: "up and over left.gif", duration: "30", id: "bny56734623656" },
+      { name: "triangle leg raise (L)", imgSrc: "triangle leg raise left.gif", duration: "30", id: "nj58g34526f" },
+      { name: "rest", imgSrc: "rest.gif", duration: "10", id: "nmn678bv35626" },
+
+      { name: "side plank leg lift (R)", imgSrc: "side plank leg lift right.gif", duration: "30", id: "q346q6bvq5" },
+      { name: "rest", imgSrc: "rest.gif", duration: "5", id: "4679jn35365" },
+      { name: "lower leg lift (R)", imgSrc: "lower leg lift right.gif", duration: "30", id: "wey54ke5737" },
+      { name: "upper leg circle (R)", imgSrc: "upper leg circle right.gif", duration: "30", id: "6w547kmwwwwnj5" },
+      { name: "rest", imgSrc: "rest.gif", duration: "5", id: "q25huyetueie" },
+      { name: "heel press leg lift (R)", imgSrc: "heel press leg lift right.gif", duration: "30", id: "wer63457jsw" },
+      { name: "knee touch extension (R)", imgSrc: "knee touch extension right.gif", duration: "30", id: "568emnw5ywy4" },
+      { name: "rest", imgSrc: "rest.gif", duration: "5", id: "6nwysdhsfsf" },
+      { name: "up and over (R)", imgSrc: "up and over right.gif", duration: "30", id: "e6wnsadfhadfh" },
+      { name: "triangle leg raise (R)", imgSrc: "triangle leg raise right.gif", duration: "30", id: "ar5ujsfgafta" },
+      { name: "rest", imgSrc: "rest.gif", duration: "10", id: "6ejgfjsfhaerer" },
+
+      { name: "side leg raise (L)", imgSrc: "side leg raise left.gif", duration: "30", id: "h5t6746jjwyw" },
+      { name: "side leg raise (R)", imgSrc: "side leg raise right.gif", duration: "30", id: "94356ybybb245" },
+      { name: "rest", imgSrc: "rest.gif", duration: "5", id: "35673657bn3" },
+      { name: "rainbow (L)", imgSrc: "rainbow left.gif", duration: "30", id: "jmdgyteyejes55" },
+      { name: "rainbow (R)", imgSrc: "rainbow right.gif", duration: "30", id: "45qhfhrwtuwu" },
+      { name: "rest", imgSrc: "rest.gif", duration: "5", id: "7jejhnwr5ywhh" },
+      { name: "fire hydrant (L)", imgSrc: "fire hydrant left.gif", duration: "30", id: "hw458ygrh2w" },
+      { name: "fire hydrant (R)", imgSrc: "fire hydrant right.gif", duration: "30", id: "34tgqagqerg46556" },
+      { name: "rest", imgSrc: "rest.gif", duration: "5", id: "f34gf34fgth6uu" },
+      { name: "single leg circle (L)", imgSrc: "single leg circle left.gif", duration: "30", id: "56n678j546hj" },
+      { name: "single leg circle (R)", imgSrc: "single leg circle right.gif", duration: "30", id: "wrgr6735735hj" },
+      { name: "rest", imgSrc: "rest.gif", duration: "10", id: "hjuikt867846" },
+
+      { name: "plie", imgSrc: "plie.gif", duration: "30", id: "76846kj46373" },
+      { name: "standing gate open (L)", imgSrc: "standing gate open left.gif", duration: "30", id: "ghjyui7iigdhd" },
+      { name: "standing gate open (R)", imgSrc: "standing gate open right.gif", duration: "30", id: "432gshtyu78jy" },
+      { name: "leg sweep (L)", imgSrc: "leg sweep left.gif", duration: "30", id: "679k5435yhh" },
+      { name: "leg sweep (R)", imgSrc: "leg sweep right.gif", duration: "30", id: "889879jhnfshs" },
+      { name: "lunge taps", imgSrc: "lunge taps.gif", duration: "30", id: "657356uhgfghg" }
+    ]
   }
 ];
 
